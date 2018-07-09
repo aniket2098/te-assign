6913bb9e7b35d487a63a3f008befc64bcf27ef2e
aniket2098 <36273744+aniket2098@users.noreply.github.com>
2018-07-09 10:57:33 +0530

    Update child.c

--- child.c
+++ child.c
@@ -8,12 +8,13 @@ int cmpfunc (const void * a, const void * b){
    return ( *(int*)a - *(int*)b );
 }
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-	int i;
-	printf("%d",atoi(argv[1]));  	
- 	//printf("%d",getpid());	
-  	qsort((int*)argv[1], 5, sizeof(int), cmpfunc);
+	printf("in child");
+	printf("%d",*(*argv+8));
+//	printf("%s",ptr);
+ 	//printf("%d",getpid());
+  	//qsort((int*)argv[1], 5, sizeof(int), cmpfunc);
   //for(i=0;i<5;i++)
 //  printf("%d\n",argv[1][i]);			
   	//printf("%d",getppid());
