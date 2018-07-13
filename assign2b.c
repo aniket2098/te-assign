b2f76f70548b1ca590b4906a9819081013f7bdb8
aniket2098 <36273744+aniket2098@users.noreply.github.com>
2018-07-13 13:10:58 +0530

    Update assign2b.c

--- assign2b.c
+++ assign2b.c
@@ -3,33 +3,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
-
-
+static int myCompare (const void * a, const void * b)
+{
+    return strcmp (*(const char **) a, *(const char **) b);
+}
 int main()
 {
-  int i,values[5] = { 98, 56, 100, 2, 25 };
+  int i;
+  char* values[5];
+
+  for( i=0;i<5;i++)
+  {
+  	values[i]=(char*)malloc(sizeof(char)*10);
+  	scanf("%s",values[i]);
+  }
+
   pid_t cpid;
   char buf;
+
   cpid = fork();
   if (cpid == -1)
   {
   	perror("fork");
     exit(EXIT_FAILURE);
   }
-
-  if (cpid == 0) 
+  if (cpid > 0) 
   {
-  	char* ptr;
-  	ptr=(char*)values;
-  	char** ptr1=&ptr;
-  	printf("%d",*(*ptr1+4));
-  	//char** ptr1=&ptr;
-  	sleep(1);
-  	execv("./c",ptr1);
+  	wait(NULL);
+  	qsort (values,5,sizeof (const char *), myCompare);            
+  	
   }
-   else 
-   {  
-    
-   }
+  else 
+  { 
+  	sleep(1);
+  	execv("./c",values);  		
+  }
 }
